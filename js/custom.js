c8e9a17b77f89b965cb3a36e5e7b3d62249b48ca
nihalCk <117338438+nihalCk@users.noreply.github.com>
2024-04-07 21:00:31 +0530

    comleted

---
+++ js/custom.js
@@ -0,0 +1,39 @@
+var glide = new Glide("#intro", {
+  type: "carousel",
+  perView: 1,
+  gap: 40,
+  autoplay: 2000,
+  pagination: {
+    el: ".glide__bullet",
+  },
+  arrows: {
+    prev: ".slider-prev",
+    next: ".slider-next",
+  },
+});
+glide.mount();
+var glide2 = new Glide("#intro2", {
+  type: "carousel",
+  perView: 2.5,
+  gap: 10,
+  
+  pagination: {
+    el: ".glide__bullet",
+  },
+  breakpoints: {
+    767: {
+      perView: 1,
+    },
+    1199: {
+      perView: 2,
+    },
+    1600: {
+      perView: 2,
+    },
+  },
+  arrows: {
+    prev: ".slider-prev",
+    next: ".slider-next",
+  },
+});
+glide2.mount();
