a42f35bf25b70bbcecd9c99dd9ba33dabbf6825a
nihalCk <117338438+nihalCk@users.noreply.github.com>
2024-04-12 17:17:41 +0530

    Updated changes

--- js/custom.js
+++ js/custom.js
@@ -3,6 +3,8 @@ var glide = new Glide("#intro", {
   perView: 1,
   gap: 40,
   autoplay: 2000,
+  hoverpause: false,
+  animationDuration: 800,
   pagination: {
     el: ".glide__bullet",
   },
@@ -12,9 +14,12 @@ var glide = new Glide("#intro", {
   },
 });
 glide.mount();
+
 var glide2 = new Glide("#intro2", {
   type: "carousel",
+  hoverpause: false,
   perView: 2.5,
+  animationDuration: 800,
   gap: 10,
   autoplay: 2000,
   pagination: {
@@ -37,3 +42,26 @@ var glide2 = new Glide("#intro2", {
   },
 });
 glide2.mount();
+
+
+new Glide('.glide4', {
+  type: 'carousel',
+  startAt: 0,
+  perView: 3,
+  focusAt: 'center',
+  gap: 0,
+  autoplay: 1000,
+  hoverpause: true,
+  animationDuration: 800,
+  rewind: true,
+  direction: 'ttb', // additional option to control the direction
+  breakpoints: {
+    768: {
+      perView: 1
+    }
+  }
+}).mount();
+
+
+
+
